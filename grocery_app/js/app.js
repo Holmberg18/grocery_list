d4a89a56c04de43f0b1e47c86865b7666614b0d3
ecin1jhol1 <jon.holmberg@ecinternet.com>
2016-07-17 22:29:59 -0700

    new js content

--- grocery_app/js/app.js
+++ grocery_app/js/app.js
@@ -1,16 +1,27 @@
-var item = '';
+var items = '';
 
 $(document).ready(function() {
-	$('.add-item_btn').mousedown(function() {
-		$(".things-i-need ul").append("<li>")
+	
+	$(".add-item").submit(function() {
+		var text = $(".add-item_input").val();
 		
 		
-		
-	});
+		var li = $('<li><input type="checkbox" class="check-item">' + text + '<img src="images/trash.png"></li>'); //idea to add an trash icon to delete 
+		li.find('.check-item').click(function(){
+			var itemli = $(this).parent();
+			$('.things-i-have #second-list').append(itemli);
+			$(this).addClass('hidden')
+		});
+		var liJquery = $(document.createElement("li")).text(text);
+		$(".things-i-need ul").append(li);
+		return false;
+	})
 	
-	
-	
+	$(".check-item")
 	
 });
+		
+	
+
 
 // $("ul#theList").append("<li><a href='url-here'>Link Text</a></li>");
