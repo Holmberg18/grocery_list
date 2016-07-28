9217a9ab4ff9f801d76fa59e2f1d0cc10844dd21
ecin1jhol1 <jon.holmberg@ecinternet.com>
2016-07-27 20:34:51 -0700

    JSON practice

--- grocery_app/js/app.js
+++ grocery_app/js/app.js
@@ -1,11 +1,24 @@
-var items = '';
+var items = [];
 
 $(document).ready(function() {
+	if(localStorage.items)items = JSON.parse(localStorage.items);//checking for localStorage existence.
+	
+	if(!items || !Array.isArray(items)) items = []; //if no items, make array or if not array, make an array out items.
+	
+
 	
 	$(".add-item").submit(function() {
 		var text = $(".add-item_input").val();
+		items.push(text);
+		localStorage.items = JSON.stringify(items);//add items as an object which we
 		
+		add(text);	
+		return false;
 		
+	
+		
+	})
+	var add = function(text){
 		var li = $('<li><input type="checkbox" class="check-item">' + text + '<img src="images/trash.png"></li>'); //idea to add an trash icon to delete 
 		li.find('.check-item').click(function(){
 			var itemli = $(this).parent();
@@ -14,14 +27,14 @@ $(document).ready(function() {
 		});
 		var liJquery = $(document.createElement("li")).text(text);
 		$(".things-i-need ul").append(li);
-		$("img").mousedown(function() { 
+		$("img").click(function() { 
 		$(this).parent().remove(); 
 		});
-		return false;
-		
+	}
 	
-		
-	})
+		for(i = 0; i < items.length; i++){
+		add(items[i]); 
+	}
 	
 	
 });
